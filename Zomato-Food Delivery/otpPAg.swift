d187ac3470a4c6f3b8385764ce1b9d0af6e5bfc3
aniket dhandhukiya <addhandhukiya2309@gmail.com>
2023-07-14 11:58:34 +0530

    co

--- Zomato-Food Delivery/otpPAg.swift
+++ Zomato-Food Delivery/otpPAg.swift
@@ -1,9 +1,4 @@
-//
-//  otpPAge.swift
-//  Zomato-Food Delivery
-//
-//  Created by R83 on 19/04/23.
-//
+
 
 import UIKit
 import UserNotifications
@@ -11,13 +6,15 @@ import UserNotifications
 class otpPAg: UIViewController {
     
     var otpArray = ["5625","5325","4679","5164","9652","5346","9564","2549","7921","5652","7941","6794","5613","7894","8945","1597"]
+    var random = ""
 
     
     @IBOutlet weak var otpView: DPOTPView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        random = otpArray.randomElement()!
     }
     
     @IBAction func otpContinueAction(_ sender: Any) {
@@ -25,6 +22,15 @@ class otpPAg: UIViewController {
             let alert = UIAlertController.init(title: "OTP", message: "Please Enter a OTP", preferredStyle: .alert)
             alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
+        }
+        else if otpView.text != random {
+            let alert = UIAlertController.init(title: "OTP", message: "Please Enter a Valid OTP", preferredStyle: .alert)
+            alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        else  {
+            let nv = storyboard?.instantiateViewController(withIdentifier: "zomatoMainPag") as! zomatoMainPag
+                navigationController?.pushViewController(nv, animated: true)
         }
 //        if (otpArray.randomElement() != nil) == otpView.validate(){
 //            let nv = storyboard?.instantiateViewController(withIdentifier: "zomatoMainPag") as! zomatoMainPag
@@ -39,11 +45,11 @@ class otpPAg: UIViewController {
         UNUserNotificationCenter.current().requestAuthorization(options: authoptions) { success, error in
             print("error", error?.localizedDescription)
         }
+        
         let content = UNMutableNotificationContent()
         content.title = "Zomato"
-        content.body = "otp = \(otpArray.randomElement()!)"
+        content.body = "otp = \(random)"
         content.badge = 1
-        
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         
         let id = UUID().uuidString
